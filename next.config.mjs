910f62651958232d85e22bc4002e691949c4cef3
Prathamesh Ssnkhe <pmsankhe_b23@it.vjti.ac.in>
2025-10-11 18:21:40 +0530

    chore: add crypto-browserify and stream-browserify dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,34 @@
 /** @type {import('next').NextConfig} */
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const nextConfig = {
   output: 'standalone',
   images: {
     domains: ["lh3.googleusercontent.com","images.unsplash.com", "assets.aceternity.com"],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        http: false,
+        https: false,
+        net: false,
+        tls: false,
+        fs: false,
+        crypto: false,
+        stream: false,
+        url: false,
+        zlib: false,
+        querystring: false,
+      };
+    }
+    return config;
   },
 };
 
